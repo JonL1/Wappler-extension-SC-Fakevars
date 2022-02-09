48f996b87dd76bccf21cd6ab5fa7310f715d3a5c
Jonas L <1325712+JonL1@users.noreply.github.com>
2022-02-09 12:07:49 +0100

    Added $_PARAM variable

--- fakevars.js
+++ fakevars.js
@@ -5,16 +5,22 @@ module.exports = {
             if (options.getvars) {
                 this.set({
                     $_GET: options.getvars
-                });
+                })
             }
             if (options.postvars) {
                 this.set({
                     $_POST: options.postvars
-                });
+                })
+            }
+            if (options.paramvars) {
+                this.set({
+                    $_PARAM: options.paramvars
+                })
             }
             return {
                 "$_GET": this.global.data.$_GET,
-                "$_POST": this.global.data.$_POST
+                "$_POST": this.global.data.$_POST,
+                "$_PARAM": this.global.data.$_PARAM
             }
         }
     }
